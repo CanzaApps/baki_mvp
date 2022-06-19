a5753e433da6ae956d3cf13800d2284f457117e3
Deon Achuo <sird655@gmail.com>
2022-06-19 11:27:17 +0100

    initial commit

---
+++ src/pages/Home.tsx
@@ -0,0 +1,11 @@
+import React, { FC } from "react";
+
+const Home: FC = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">Home</h1>
+    </div>
+  );
+};
+
+export default Home;
